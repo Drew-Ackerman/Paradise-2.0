407b6c23ed79e3f4937a3c759bc1083a7bdda50e
Neommastermind <MatthewKJensen.live@gmail.com>
2017-11-20 10:39:54 -0700

    Added a dropdown for the donorLevels

--- Paradise/Paradise/Controllers/AboutUsController.cs
+++ Paradise/Paradise/Controllers/AboutUsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -84,6 +85,7 @@ namespace Paradise.Controllers
             {
                 MailMessage mailMessage = new MailMessage(email, "MatthewJensen7@mail.weber.edu", "Volunteering", message);
                 SmtpClient mail = new SmtpClient("smtp.gmail.com");
+                mail.Credentials = CredentialCache.DefaultNetworkCredentials;
                 //Add authentication for email
                 mail.Send(mailMessage);
             }
